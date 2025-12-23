8d8683e5e1662f0b778da8c596b8e2c0f3e6a7f0
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2025-12-23 23:10:25 +0530

    fix9

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -30,13 +30,14 @@ resource "aws_security_group" "rds" {
   }
 }
 
+
 resource "aws_db_instance" "main" {
   identifier                 = "${local.prefix}-db"
   db_name                    = "recipe"
   allocated_storage          = 20
   storage_type               = "gp2"
   engine                     = "postgres"
-  engine_version             = "15.3"
+  engine_version             = "17.2"
   auto_minor_version_upgrade = true
   instance_class             = "db.t4g.micro"
   username                   = "recipeapp"
@@ -46,7 +47,7 @@ resource "aws_db_instance" "main" {
   multi_az                   = false
   backup_retention_period    = 0
   vpc_security_group_ids     = [aws_security_group.rds.id]
-
+ 
   tags = {
     Name = "${local.prefix}-main"
   }
