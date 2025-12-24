15e24829b0f17f67acc2af9e426be16af267d3b9
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2025-12-24 22:53:03 +0530

    reverted

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -120,7 +120,7 @@ resource "aws_ecs_task_definition" "api" {
         environment = [
           {
             name  = "APP_HOST"
-            value = "api"
+            value = "127.0.0.1"
           }
         ]
         mountPoints = [
