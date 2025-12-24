6230f7f90fb7d6df54cba8c66b19e011fe4b9f9c
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2025-12-24 22:24:28 +0530

    reverted

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-
 set -e
+
+echo "Waiting for API to be available..."
+
+while ! nc -z ${APP_HOST} ${APP_PORT}; do
+  sleep 2
+done
+
+echo "API is up, starting nginx"
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
