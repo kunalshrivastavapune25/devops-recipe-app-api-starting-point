ce42487eb95670747c49c487ba0af52808a453fa
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2025-12-24 18:19:39 +0530

    reverted

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -19,11 +19,14 @@ resource "aws_security_group" "rds" {
   name        = "${local.prefix}-rds-inbound-access"
   vpc_id      = aws_vpc.main.id
 
+
   ingress {
     protocol  = "tcp"
     from_port = 5432
     to_port   = 5432
-  
+    security_groups = [
+      aws_security_group.ecs_service.id
+    ]    
   }
 
   tags = {
