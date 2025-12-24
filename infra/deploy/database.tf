693934a079c50c0cdc791a3d5365ccf5b4cba12f
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2025-12-24 18:22:46 +0530

    reverted

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -19,11 +19,11 @@ resource "aws_security_group" "rds" {
   name        = "${local.prefix}-rds-inbound-access"
   vpc_id      = aws_vpc.main.id
 
-
   ingress {
     protocol  = "tcp"
     from_port = 5432
     to_port   = 5432
+
     security_groups = [
       aws_security_group.ecs_service.id
     ]    
@@ -40,7 +40,7 @@ resource "aws_db_instance" "main" {
   allocated_storage          = 20
   storage_type               = "gp2"
   engine                     = "postgres"
-  engine_version             = "15.7"
+  engine_version             = "15.3"
   auto_minor_version_upgrade = true
   instance_class             = "db.t4g.micro"
   username                   = var.db_username
