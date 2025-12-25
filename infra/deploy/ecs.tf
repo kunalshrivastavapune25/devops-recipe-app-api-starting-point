6f3bedf84c8df7fa56d986b845f624d333bf263d
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2025-12-25 10:46:42 +0530

    lb

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -199,7 +199,6 @@ resource "aws_ecs_service" "api" {
   enable_execute_command = true
 
   network_configuration {
-    assign_public_ip = true
 
     subnets = [
       aws_subnet.private_a.id,
@@ -207,6 +206,11 @@ resource "aws_ecs_service" "api" {
     ]
 
     security_groups = [aws_security_group.ecs_service.id]
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api.arn
+    container_name   = "proxy"
+    container_port   = 8000
   }
 }
 
